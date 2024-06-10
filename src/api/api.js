169f7277da2e1e200e6d25b209f72f62e7257398
vashnavichauhan1825 <vashnavichauhan1@gmail.com>
2024-06-11 00:48:43 +0530

    added env

--- src/api/api.js
+++ src/api/api.js
@@ -1,13 +1,10 @@
 import axios from "axios";
 
-const API_KEY = "8cc28e952emshc81f8cf9a48f788p1ffa67jsn9b46356be88f";
-const BASE_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
-
 const apiClient = axios.create({
-  baseURL: BASE_URL,
+  baseURL: process.env.VUE_APP_BASE_URL,
   headers: {
     "x-rapidapi-host": "wft-geo-db.p.rapidapi.com",
-    "x-rapidapi-key": API_KEY,
+    "x-rapidapi-key": process.env.VUE_APP_API_KEY,
   },
 });
 
